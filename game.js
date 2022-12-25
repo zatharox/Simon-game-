563f50761bdd58dc6e0e2c13bffefae241f67976
zatharox <104523070+zatharox@users.noreply.github.com>
2022-12-25 15:54:28 +0530

    Update game.js

--- game.js
+++ game.js
@@ -67,7 +67,7 @@ function animatePress(currentColor) {
 }
 
 function playSound(name) {
-  var audio = new Audio("sounds/" + name + ".mp3");
+  var audio = new Audio(name + ".mp3");
   audio.play();
 }
 
